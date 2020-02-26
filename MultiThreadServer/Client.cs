4444ad11546c4ef94f668ae12211dc2fb806e898
Vincent Cottin <vincent@lagenhetsbyte.se>
2020-02-26 16:24:22 +0100

    Initial commit

---
+++ MultiThreadServer/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MultiThreadServer
+{
+    class Client : IDisposable
+    {
+        public TcpClient TcpClient { get; set; }
+        public string Name { get; set; }
+
+        public Client(TcpClient client)
+        {
+            TcpClient = client;
+        }
+
+        public void SendMessage(string message)
+        {
+            var stream = TcpClient.GetStream();
+
+            // Skicka meddelandets storlek (i byte array)
+            var bytes = BitConverter.GetBytes(message.Length);
+            stream.Write(bytes, 0, bytes.Length);
+
+            // Gör meddelandet till bytearray och skicka
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            stream.Write(messageBytes, 0, messageBytes.Length);
+
+        }
+
+        public string WaitForMessage()
+        {
+            var stream = TcpClient.GetStream();
+
+            // Lyssna på hur stort inkommande paket är
+            var sizeBuffer = new byte[10];
+            stream.Read(sizeBuffer, 0, sizeBuffer.Length);
+
+            var packageSize = BitConverter.ToInt32(sizeBuffer, 0);
+
+            // Skapa en array med den storleken
+            var messageBuffer = new byte[packageSize];
+
+            // Läs den storleken
+            stream.Read(messageBuffer, 0, messageBuffer.Length);
+            
+            // Convertera bytearray till string
+            var message = Encoding.UTF8.GetString(messageBuffer);
+            return message;
+        }
+
+        public void Dispose()
+        {
+            TcpClient.Close();
+        }
+    }
+}
---
+++ MultiThreadServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace MultiThreadServer
+{
+    class Program
+    {
+        public static List<Client> ConnectedClients = new List<Client>();
+
+        private static void ProcessClientrequests(Client client)
+        {
+            try
+            {
+                client.SendMessage("You have been connected to the server");
+                client.SendMessage("Enter your name:");
+                client.Name = client.WaitForMessage();
+
+                while (true)
+                {
+                    var message = client.WaitForMessage();
+                    foreach (var c in ConnectedClients)
+                    {
+                        c.SendMessage(client.Name + ": " + message);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Problem with client communication. Exiting thread " + e);
+                client.Dispose();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = null;
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8001);
+                listener.Start();
+                Console.WriteLine("Multi threader started...");
+
+                while(true)
+                {
+                    Console.WriteLine("Waiting for incoming client connections...");
+                    var tcpClient = listener.AcceptTcpClient();
+
+                    var client = new Client(tcpClient);
+                    ConnectedClients.Add(client);
+
+                    //startar en ny tråd (Task.Run)
+                    Task.Run(() => ProcessClientrequests(client));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
